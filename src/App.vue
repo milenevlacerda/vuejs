401bf9f117aed756afc865b45bb9cf115f2e7d14
milenevlacerda <milenelacerda@icloud.com>
2017-03-20 15:52:22 -0300

    Modificando App.vue para exemplo de lista de fotos

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
+    <h1> {{ title }} </h1>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li v-for="picture in pictures">
+        <img v-bind:src="picture.url" v-bind:alt="picture.title">
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      title: 'Pictures'
+      pictures: [
+        {
+          url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiZxJZkHv2sGQRxue11kOAzGHRDkByt9Yu0mREYpNrxxm8laEZuQEdPWc',
+          title: 'dog'
+        },
+        {
+          url: 'http://www.ultracurioso.com.br/wp-content/uploads/2015/08/Cat.jpg',
+          title: 'cat'
+        }
+      ]
     }
   }
 }
@@ -38,23 +37,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
