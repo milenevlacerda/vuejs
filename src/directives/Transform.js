6669aa0cd0de1fe8623d7c5746d0642639ef15b8
Milene Vieira Lacerda <milenelacerda@icloud.com>
2017-05-27 20:25:28 -0300

    Adicionando lógica de diretiva, modificadores e argumentos

--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -5,8 +5,26 @@ Vue.directive( 'transform', {
 
         let current = 0;
         el.addEventListener('dblclick', function(){
-            current+=90;
-            el.style.transform = `rotate(${current}deg)`;
+            let increment = binding.value || 90;
+            let efeito;
+
+            if( !binding.arg || binding.arg == 'rotate' ) {
+                if( binding.modifiers.reverse ) {
+                    current-=increment;
+                } else {
+                    current+=increment;
+                }
+                efeito = `rotate(${current}deg)`
+            }else if( binding.arg == 'scale' ){
+                efeito = `scale(${increment})`
+            }
+            
+            el.style.transform = efeito;
+
+            if( binding.modifiers.animate ) {
+                this.style.transition = 'transform 0.5s';
+            }
+            
         });
     }
 });
